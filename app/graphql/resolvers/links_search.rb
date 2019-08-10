ca47f2e34643ebc01e69a02442a202547e02564d
Pablo Levy <pablo@busolinea.com>
2019-08-10 10:32:02 -0500

    add pagination

--- app/graphql/resolvers/links_search.rb
+++ app/graphql/resolvers/links_search.rb
@@ -14,6 +14,8 @@ class Resolvers::LinksSearch
   end
 
   option :filter, type: LinkFilter, with: :apply_filter
+  option :first, type: types.Int, with: :apply_first
+  option :skip, type: types.Int, with: :apply_skip
 
   def apply_filter(scope, value)
     branches = normalize_filters(value).reduce { |a,b| a.or(b) }
@@ -31,5 +33,13 @@ class Resolvers::LinksSearch
     value[:OR].reduce(branches) { |s,v| normalize_filters(v,s) } if value[:OR].present?
 
     branches
+  end
+
+  def apply_first(scope, value)
+    scope.limit(value)
+  end
+
+  def apply_skip(scope, value)
+    scope.offset(value)
   end
 end
